cf47ade9082929353bb82d910e297a2617e39897
Federico Grandi <fgrandi30@gmail.com>
2019-12-19 21:50:59 +0100

    Better error handling

--- src/main.ts
+++ src/main.ts
@@ -9,29 +9,34 @@ const token = core.getInput('token'),
   eventFile = process.env.GITHUB_EVENT_PATH || '/github/workflow/event.json';
 
 (async function main() {
-  const eventObj = await readJson(eventFile),
-    tag = (await getTag(eventObj.head)).tag
+  try {
+    const eventObj = await readJson(eventFile),
+      tag = (await getTag(eventObj.head)).tag
 
-  let match = '',
-    major = ''
-  if (typeof tag == 'string' && semverRegex().test(tag)) {
-    match = (tag.match(semverRegex()) || [''])[0]
-    if (match) major = match[0]
-  } else return core.info('No tag matching the SemVer regex has been found, no tags have been created.')
+    let match = '',
+      major = ''
+    if (typeof tag == 'string' && semverRegex().test(tag)) {
+      match = (tag.match(semverRegex()) || [''])[0]
+      if (match) major = match[0]
+    } else return core.info('No tag matching the SemVer regex has been found, no tags have been created.')
 
-  if (major) {
-    const tagProcess = spawn(`git tag --force -a v${major} -m "Link to version ${match}"`)
-    tagProcess.stdout.on('data', d => console.log(`tag: ${d}`))
-    tagProcess.on('exit', code => {
-      if (code != 0) core.setFailed(`The tag process failed with code ${code}. More info is probably written above.`)
-      if (!!token && (!push || push == 'true')) {
-        const pushProcess = spawn('git push --tags')
-        pushProcess.stdout.on('data', d => console.log(`push: ${d}`))
-        pushProcess.on('exit', code => {
-          if (code != 0) core.setFailed(`The push process failed with code ${code}. More info is probably written above.`)
-        })
-      } else if (!token) core.setFailed('Although a match has been found, you requested to push the created tags but didn\'t provide a token.')
-    })
+    if (major) {
+      const tagProcess = spawn(`git tag --force -a v${major} -m "Link to version ${match}"`)
+      tagProcess.stdout.on('data', d => console.log(`tag: ${d}`))
+      tagProcess.on('exit', code => {
+        if (code != 0) core.setFailed(`The tag process failed with code ${code}. More info is probably written above.`)
+        if (!!token && (!push || push == 'true')) {
+          const pushProcess = spawn('git push --tags')
+          pushProcess.stdout.on('data', d => console.log(`push: ${d}`))
+          pushProcess.on('exit', code => {
+            if (code != 0) core.setFailed(`The push process failed with code ${code}. More info is probably written above.`)
+          })
+        } else if (!token) core.setFailed('Although a match has been found, you requested to push the created tags but didn\'t provide a token.')
+      })
+    }
+  } catch (e) {
+    core.error(e);
+    core.setFailed(e);
   }
 })()
 
