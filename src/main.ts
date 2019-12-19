c9ca18cfe35904849cedf017b7f0855e2e04bf02
Federico Grandi <fgrandi30@gmail.com>
2019-12-19 22:20:54 +0100

    Update dist (#2)
    * Better error handling
    
    * Use octonode instead of axios
    
    * Formatting changes
    
    * [auto] Update compiled version

--- src/main.ts
+++ src/main.ts
@@ -1,61 +1,71 @@
-import axios from 'axios'
-import semverRegex from 'semver-regex';
 import * as core from '@actions/core'
-import { readFile } from 'fs';
-import { spawn } from 'child_process';
+import * as github from 'octonode'
+import semverRegex from 'semver-regex'
+import { readFile } from 'fs'
+import { spawn } from 'child_process'
 
 const token = core.getInput('token'),
   push = core.getInput('push'),
-  eventFile = process.env.GITHUB_EVENT_PATH || '/github/workflow/event.json';
+  eventFile = process.env.GITHUB_EVENT_PATH || '/github/workflow/event.json'
+
+const client = github.client(token || undefined),
+  repo = client.repo(process.env.GITHUB_REPOSITORY);
 
 (async function main() {
-  const eventObj = await readJson(eventFile),
-    tag = (await getTag(eventObj.head)).tag
+  try {
+    const eventObj = await readJson(eventFile),
+      tag = await getTag(eventObj.head)
 
-  let match = '',
-    major = ''
-  if (typeof tag == 'string' && semverRegex().test(tag)) {
-    match = (tag.match(semverRegex()) || [''])[0]
-    if (match) major = match[0]
-  } else return core.info('No tag matching the SemVer regex has been found, no tags have been created.')
+    let match = '',
+      major = ''
+    if (typeof tag == 'string' && semverRegex().test(tag)) {
+      match = (tag.match(semverRegex()) || [''])[0]
+      if (match) major = match[0]
+    } else return core.info('No tag matching the SemVer regex has been found, no tags have been created.')
 
-  if (major) {
-    const tagProcess = spawn(`git tag --force -a v${major} -m "Link to version ${match}"`)
-    tagProcess.stdout.on('data', d => console.log(`tag: ${d}`))
-    tagProcess.on('exit', code => {
-      if (code != 0) core.setFailed(`The tag process failed with code ${code}. More info is probably written above.`)
-      if (!!token && (!push || push == 'true')) {
-        const pushProcess = spawn('git push --tags')
-        pushProcess.stdout.on('data', d => console.log(`push: ${d}`))
-        pushProcess.on('exit', code => {
-          if (code != 0) core.setFailed(`The push process failed with code ${code}. More info is probably written above.`)
-        })
-      } else if (!token) core.setFailed('Although a match has been found, you requested to push the created tags but didn\'t provide a token.')
-    })
+    if (major) {
+      const tagProcess = spawn(`git tag --force -a v${major} -m "Link to version ${match}"`)
+      tagProcess.stdout.on('data', d => console.log(`tag: ${d}`))
+      tagProcess.on('exit', code => {
+        if (code != 0) core.setFailed(`The tag process failed with code ${code}. More info is probably written above.`)
+        if (!!token && (!push || push == 'true')) {
+          const pushProcess = spawn('git push --tags')
+          pushProcess.stdout.on('data', d => console.log(`push: ${d}`))
+          pushProcess.on('exit', code => {
+            if (code != 0) core.setFailed(`The push process failed with code ${code}. More info is probably written above.`)
+          })
+        } else if (!token) core.setFailed('Although a match has been found, you requested to push the created tags but didn\'t provide a token.')
+      })
+    }
+  } catch (e) {
+    core.error(e)
+    core.setFailed(e)
   }
 })()
 
 async function readJson(file: string) {
   const data: string = await new Promise((resolve, reject) =>
     readFile(file, "utf8", (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
+      if (err) reject(err)
+      else resolve(data)
     })
-  );
-  return JSON.parse(data);
+  )
+  return JSON.parse(data)
 }
 
-async function getTag(sha: string): Promise<tagInfo> {
-  const url = `https://api.github.com/repos/${process.env.GITHUB_REPOSITORY}/tags/${sha}`,
-    headers = token ? {
-      Authorization: `Bearer ${token}`
-    } : undefined
-
-  return (await axios.get(url, { headers }))
+async function getTag(sha: string): Promise<string | undefined> {
+  const tags: tagInfo[] = (await repo.tagsAsync())[0]
+  for (let tag of tags) {
+    if (tag.commit.sha == sha) return tag.name
+  }
 }
 interface tagInfo {
-  tag: string
+  commit: {
+    sha: string
+    url: string
+  }
+  name: string
+  node_id: string
+  tarball_url: string
+  zipball_url: string
 }
